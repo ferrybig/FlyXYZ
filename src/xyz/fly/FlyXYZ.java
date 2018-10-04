6e4c9688c8129a82a6740022640c21eec8b8d378
EssentialXYZ <essential@grogers.xyz>
2018-10-04 15:29:25 +1300

    Add Version 1.0

---
+++ src/xyz/fly/FlyXYZ.java
@@ -0,0 +1,47 @@
+package xyz.fly;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+
+public class FlyXYZ extends JavaPlugin {
+
+    private ArrayList<Player> players;
+    private String prefix = ChatColor.WHITE + "[" + ChatColor.GOLD + "Fly" + ChatColor.WHITE + "] " + ChatColor.GRAY;
+
+    public FlyXYZ(){
+        players = new ArrayList<>();
+    }
+
+    @Override
+    public void onDisable(){
+        for(Player p: this.players){
+            p.setAllowFlight(false);
+        }
+    }
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String args[]){
+        Player player = (Player) sender;
+        if(command.getName().equalsIgnoreCase("fly")){
+            if(!sender.hasPermission("xyz.fly")){
+                sender.sendMessage(ChatColor.DARK_RED + "You Dont Have Permissions For Using This Command.");
+                return true;
+            }
+            if(!player.getAllowFlight()){
+                player.setAllowFlight(true);
+                player.sendMessage(prefix + ChatColor.GRAY + "Enabled!");
+                return true;
+            }
+            player.setAllowFlight(false);
+            player.sendMessage(prefix + ChatColor.GRAY + "Disabled!");
+            return true;
+        }
+        return true;
+    }
+}
